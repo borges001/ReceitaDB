8e83cbdcf72544e2cd66bcdcc4f29328206b0998
Cleiton Borges <96124341+borges001@users.noreply.github.com>
2024-05-28 17:04:34 -0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ DOWNLOAD_DIR="Downloads"
 DATA_DIR="Data"
 URL="http://200.152.38.155/CNPJ/"  # Substitua pelo URL real
 
-# Alvos
+# Alvo
 download() {
     echo "Iniciando download..."
     wget -r -np -nH --cut-dirs=1 -R "index.html*" -P "$DOWNLOAD_DIR" "$URL"
@@ -21,25 +21,9 @@ create_data_dir() {
     mkdir -p "$DATA_DIR"
 }
 
-clean() {
-    echo "Limpando diretórios..."
-    rm -rf "$DOWNLOAD_DIR" "$DATA_DIR"
-}
-
 # Execução
-case "$1" in
-    "download")
-        create_download_dir
-        create_data_dir
-        download
-        ;;
-    "clean")
-        clean
-        ;;
-    *)
-        echo "Uso: $0 {download|clean}"
-        exit 1
-        ;;
-esac
+create_download_dir
+create_data_dir
+download
 
 exit 0
